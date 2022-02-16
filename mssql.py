169b9c195fe32318685340040150fce4d18875c4
Tobias <75634835+LilBondz@users.noreply.github.com>
2022-02-16 09:36:54 +0100

    Update mssql.py

--- mssql.py
+++ mssql.py
@@ -1,23 +1,33 @@
+# Importerer SQL modulen til python
 import pymssql
 
+# Lager variabel for Tilkoblinksinfo
 server = "192.168.15.15\\IT22"
+# Brukernavnet vi logger inn i databasen med
 user = "sa"
+# Passordet til brukeren
 password = "123QWEr"
 
-connect = pymssql.connect(server, user, password, "datob")
+# Variabel som kobler seg til SQL serveren med info variablene
+connect = pymssql.connect(server, user, password, "dbtob")
+# lager en variabel til styring av databasen
 exe = connect.cursor()
-exe.execute("IF OBJECT_ID('ansatt', 'U') IS NULL CREATE TABLE ansatt (ansattnr INT NOT NULL IDENTITY PRIMARY KEY, fornavn VARCHAR(100), etternavn VARCHAR(100), PRIMARY KEY(id))")
-running = True
-id
-while running:
-    fornavn = input("Skriv inn fornavn: ")
-    etternavn = input("Skriv inn etternavn: ")
-    exe.execute(f"INSERT INTO ansatt VALUES ({fornavn}, {etternavn}")
-    again = input("Legg til en til? y/n")
-    if again == "y":
-        pass
-    else:
-        running = False
+# skriver inn tekskommandoer som blir kjørt på SQL serveren
+# Den lager en database hvis den ikke er der med id, fornavn og etternavn kolonner
+exe.execute("IF OBJECT_ID('ansatt', 'U') IS NULL CREATE TABLE ansatt (id INT NOT NULL IDENTITY PRIMARY KEY, fornavn VARCHAR(100), etternavn VARCHAR(100))")
+# Spør bruker om å skrive fornavn
+fornavn = input("Skriv inn fornavn: ")
+# spør bruker om å skrive etternavn
+etternavn = input("Skriv inn etternavn: ")
 
+# Lager en variabel med kommandoen som skal kjøres på serveren med infoen
+sql = f"INSERT INTO ansatt (fornavn, etternavn) VALUES ('{fornavn}', '{etternavn}')"
+# printer den først i python for å vise at teksten som blir sent til servern er rett
+print(sql)
+# Kjører kommandoen på serveren
+exe.execute(sql)
+# Kjører commit som sier utfør endringer
 connect.commit()
 
+# Lukker broen mellom klient og server
+connect.close()
